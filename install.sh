7b2732637915a3b25b7d0e2b3c58b757311f4bfe
X <yannic.niessen@gmail.com>
2021-04-30 08:50:58 +0200

    install script autostart added

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ apt-get -y upgrade
 apt-get -y install default-jdk
 apt-get -y install openjfx
 cp ./raspberrypi/boot/ /boot
- 
+mdkir ~/.config/autostart
+cp ./raspberrypi/auretim.desktop ~/.config/autostart/ 
+reboot
