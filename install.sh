ea0d1b8af2de5678774bacb7bc657c7ec283c670
X <yannic.niessen@gmail.com>
2021-05-08 23:19:18 +0200

    added maven installation to install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ apt-get -y upgrade
 apt-get -y install default-jdk
 apt-get -y install openjfx
 apt-get -y install espeak-ng
+apt-get -y install maven
 cp -r ./raspberrypi/boot/* /boot
 mkdir /home/pi/.config/autostart
 cp ./raspberrypi/auretim.desktop /home/pi/.config/autostart/
